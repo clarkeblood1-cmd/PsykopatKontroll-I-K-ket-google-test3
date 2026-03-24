f4092880d6ecc7d4f3640a6f957c443e480e9986
clarkeblood1-cmd <clarkeblood1@gmail.com>
2026-03-24 04:03:39 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,9 @@
 window.firebaseConfig = {
-  apiKey: "DIN_API_KEY",
-  authDomain: "DITT_PROJEKT.firebaseapp.com",
-  projectId: "DITT_PROJEKT_ID",
-  appId: "DIN_APP_ID"
+  apiKey: "AIzaSyBXqoG8ZaDbuY-KJwS5ClMovgy8Pb3vw54",
+  authDomain: "psykopatkontroll.firebaseapp.com",
+  projectId: "psykopatkontroll",
+  storageBucket: "psykopatkontroll.firebasestorage.app",
+  messagingSenderId: "89089537506",
+  appId: "1:89089537506:web:573383e0e2ebf4ded035bb",
+  measurementId: "G-Z8XRTGK419"
 };
