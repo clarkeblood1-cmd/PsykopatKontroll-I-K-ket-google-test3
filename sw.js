ebe9e4a4aca07168cee34d481f5243b727f05639
clarkeblood1-cmd <clarkeblood1@gmail.com>
2026-03-28 05:44:52 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,11 @@
-const CACHE_NAME = 'matlista-app-v6';
+const CACHE_NAME = 'matlista-app-v12-01';
 const ASSETS = [
   './',
   './index.html',
   './style.css',
   './script.js',
+  './cloud-sync.js',
+  './cloud-hooks.js',
   './manifest.json',
   './icons/icon-192.png',
   './icons/icon-512.png'
