e2e0437bd338caa5d83d20856f0a882d2f259be4
clarkeblood1-cmd <clarkeblood1@gmail.com>
2026-03-31 23:24:44 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'matlista-app-v72-expiry-tools';
+const CACHE_NAME = 'matlista-app-v73-appstore-ui';
 const ASSETS = [
   './',
   './index.html',
@@ -6,6 +6,7 @@ const ASSETS = [
   './script.js',
   './cloud-sync.js',
   './cloud-hooks.js',
+  './appstore-ui.js',
   './household.js',
   './firebase-config.js',
   './manifest.json',
